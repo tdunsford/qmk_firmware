96245fbbd5cd9e7c45b1f70ed7940ac43c2ce468
Trevor Dunsford <tdunsford@gmail.com>
2016-09-24 11:56:13 -0500

    adjust keys

--- keyboards/atomic/keymaps/td2/keymap.c
+++ keyboards/atomic/keymaps/td2/keymap.c
@@ -18,17 +18,6 @@ enum keyboard_macros {
   MACRO_LOWER,
   MACRO_FUNCTION,
   MACRO_MOUSE,
-  MACRO_TIMBRE_1,
-  MACRO_TIMBRE_2,
-  MACRO_TIMBRE_3,
-  MACRO_TIMBRE_4,
-  MACRO_TEMPO_U,
-  MACRO_TEMPO_D,
-  MACRO_TONE_DEFAULT,
-  MACRO_MUSIC_TOGGLE,
-  MACRO_AUDIO_TOGGLE,
-  MACRO_INC_VOICE,
-  MACRO_DEC_VOICE,
   MACRO_BACKLIGHT,
   MACRO_BREATH_TOGGLE,
   MACRO_BREATH_SPEED_INC,
@@ -73,11 +62,6 @@ enum keyboard_macros {
 #define M_HELP8             M(MACRO_HELP_8)
 #define M_HELP9             M(MACRO_HELP_9)
 
-
-#define VC_UP               M(MACRO_INC_VOICE)
-#define VC_DOWN             M(MACRO_DEC_VOICE)
-
-
 #define SC_UNDO             LCTL(KC_Z)
 #define SC_REDO             LCTL(KC_Y)
 #define SC_CUT              LCTL(KC_X)
@@ -113,12 +97,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   '--------------------------------------------------------------------------------------------------------------------------------------'
 */
 [LAYER_QWERTY] = {
-  { KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSPC, KC_BSPC },
-  { KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL  },
-  { KC_CAPS, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT , KC_ENT , KC_PGUP },
-  { KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, KC_RSFT, KC_UP  , KC_PGDN },
-  { KC_LCTL, KC_LGUI, M_FUNCT, KC_LALT, M_UPPER, KC_SPC , KC_SPC , M_LOWER, OS_SHFT, KC_RALT, KC_APP , KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT }
+  { KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC  },
+  { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL   },
+  { KC_RCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_ENT,  KC_PGUP  },
+  { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RSFT, KC_UP,   KC_PGDN  },
+  { KC_LCTL, KC_LGUI, KC_NO,   KC_LALT, M_UPPER, KC_SPC,  KC_NO,   M_LOWER, KC_RALT, KC_NO,   KC_RGUI, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT  },
 },
+
 /* LAYER = LAYER_UPPER
   .--------------------------------------------------------------------------------------------------------------------------------------.
   | PRINT  | XXXXXX | XXXXXX | XXXXXX | XXXXXX | XXXXXX | XXXXXX | NUM LK | KP /   | KP *   | KP -   | XXXXXX | XXXXXX | ______ . ______ |
@@ -132,12 +117,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   | ______ | ______ | ______ | ______ | UPPER  | KP 0   . KP 0   | ______ | RALT   | KP .   | KP ENT | ______ | ______ | ______ | ______ |
   '--------------------------------------------------------------------------------------------------------------------------------------'
 */
-[LAYER_UPPER] = {
+[LAYER_LOWER] = {
   { KC_PSCR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, XXXXXXX, XXXXXXX, _______, _______ },
   { KC_PAUS, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_NLCK, KC_PSLS, KC_KP_7, KC_KP_8, KC_KP_9, KC_PMNS, _______, _______, _______, KC_INS  },
   { _______, KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_CAPS, KC_PAST, KC_KP_4, KC_KP_5, KC_KP_6, KC_PPLS, _______, _______, _______, KC_HOME },
   { _______, KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_SLCK, KC_KP_0, KC_KP_1, KC_KP_2, KC_KP_3, KC_PENT, _______, _______, _______, KC_END  },
-  { _______, _______, _______, _______, M_UPPER, KC_KP_0, KC_KP_0, _______, KC_RALT, KC_PDOT, KC_PENT, _______, _______, _______, _______ }
+  { _______, _______, _______, _______, _______, KC_KP_0, KC_KP_0, M_LOWER, KC_RALT, KC_PDOT, KC_PENT, _______, _______, _______, _______ }
 },
 /* LAYER = LAYER_LOWER
   .--------------------------------------------------------------------------------------------------------------------------------------.
@@ -152,12 +137,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   | ______ | ______ | ______ | ______ | ______ | ______ . ______ | LOWER  | ______ | ______ | ______ | ______ | ______ | ______ | ______ |
   '--------------------------------------------------------------------------------------------------------------------------------------'
 */
-[LAYER_LOWER] = {
+[LAYER_UPPER] = {
   { KC_PSCR, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______, _______ },
   { _______, KC_DLR , KC_LCBR, KC_LBRC, KC_LPRN, KC_PERC, KC_HASH, KC_RPRN, KC_RBRC, KC_RCBR, KC_AT  , _______, _______, _______, KC_INS  },
   { _______, KC_CIRC, KC_ASTR, KC_PLUS, KC_MINS, KC_SLSH, KC_BSLS, KC_UNDS, KC_QUOT, KC_DQT , KC_GRV , _______, _______, _______, KC_HOME },
   { _______, KC_PIPE, KC_AMPR, KC_EXLM, KC_TILD, KC_SCLN, KC_COLN, KC_EQL , KC_LT  , KC_GT  , KC_QUES, _______, _______, _______, KC_END  },
-  { _______, _______, _______, _______, _______, _______, _______, M_LOWER, _______, _______, _______, _______, _______, _______, _______ }
+  { _______, _______, _______, _______, M_UPPER, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ }
 },
 /* LAYER = LAYER_FUNCTION
   .--------------------------------------------------------------------------------------------------------------------------------------.
